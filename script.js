fb65100b648b57c529d3c7da024236d35701460a
yuri nikitin <2497567@gmail.com>
2025-06-28 07:25:17 +0300

    design3

--- script.js
+++ script.js
@@ -1,19 +1,49 @@
+let map;
+
+// Инициализация карты
+function initMap() {
+    map = new ymaps.Map("map", {
+        center: [56.3264, 44.0032], // координаты клиники
+        zoom: 14,
+        controls: ['zoomControl', 'typeSelector', 'geolocationControl']
+    });
+
+    // Добавляем маркер на карту
+    var placemark = new ymaps.Placemark([56.3264, 44.0032], {}, {
+        preset: 'islands#icon',
+        iconColor: '#FF0000' // цвет маркера
+    });
+
+    map.geoObjects.add(placemark);
+}
+
+// Функция для расчета маршрута
+function calculateRoute() {
+    let address = document.getElementById('addressInput').value.trim();
+    if (!address) return alert('Укажите ваш адрес');
+
+    // Сначала определяем координаты введённого адреса
+    ymaps.geocode(address).then(res => {
+        let coords = res.geoObjects.get(0).geometry.getCoordinates();
+
+        // Объект MultiRoute для построения маршрута
+        var multiRoute = new ymaps.multiRouter.MultiRoute({
+            referencePoints: [
+                coords, // стартовая точка (адрес пользователя)
+                [56.3264, 44.0032] // конечная точка (клиника)
+            ]
+        }, {
+            boundsAutoApply: true
+        });
+
+        // Добавляем маршрут на карту
+        map.geoObjects.add(multiRoute);
+    }).catch(err => {
+        alert('Ошибка определения координат вашего адреса.');
+    });
+}
+
+// Ждём готовности DOM и инициализируем карту
 document.addEventListener('DOMContentLoaded', function () {
-    ymaps.ready(init);
-
-    function init () {
-        var myMap = new ymaps.Map("map", {
-            center: [56.3264, 44.0032], // координаты клиники
-            zoom: 14,
-            controls: ['zoomControl'], // добавляем контроль зума
-        });
-
-        // Добавляем маркер на карту
-        var placemark = new ymaps.Placemark([56.3264, 44.0032], {}, {
-            preset: 'islands#icon',
-            iconColor: '#FF0000' // цвет маркера
-        });
-
-        myMap.geoObjects.add(placemark);
-    }
+    ymaps.ready(initMap);
 });
