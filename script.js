5643199d2e9c5e480ee8e584dec01d3113e902f1
yuri nikitin <2497567@gmail.com>
2025-06-28 07:20:56 +0300

    new карта

--- script.js
+++ script.js
@@ -1,48 +1,19 @@
-let map;
+document.addEventListener('DOMContentLoaded', function () {
+    ymaps.ready(init);
 
-// Инициализация карты
-function initMap() {
-    const clinicLocation = { lat: 56.3264, lng: 44.0032 }; // координаты Нижнего Новгорода
-    map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 14,
-        center: clinicLocation,
-        mapTypeControl: true,
-        fullscreenControl: true,
-        streetViewControl: true,
-        rotateControl: true,
-        scaleControl: true,
-        zoomControlOptions: {
-            position: google.maps.ControlPosition.RIGHT_BOTTOM
-        },
-    });
+    function init () {
+        var myMap = new ymaps.Map("map", {
+            center: [56.3264, 44.0032], // координаты клиники
+            zoom: 14,
+            controls: ['zoomControl'], // добавляем контроль зума
+        });
 
-    // Ставим метку на карте с клиникой
-    const marker = new google.maps.Marker({
-        position: clinicLocation,
-        map: map,
-        title: 'Консультация ортопеда',
-    });
-}
+        // Добавляем маркер на карту
+        var placemark = new ymaps.Placemark([56.3264, 44.0032], {}, {
+            preset: 'islands#icon',
+            iconColor: '#FF0000' // цвет маркера
+        });
 
-// Строим маршрут
-function calculateRoute() {
-    let address = document.getElementById('addressInput').value.trim();
-    if (!address || !google) return alert('Укажите верный адрес');
-
-    const directionsService = new google.maps.DirectionsService();
-    const directionsRenderer = new google.maps.DirectionsRenderer({ map: map });
-
-    const request = {
-        origin: address,
-        destination: {lat: 56.3264, lng: 44.0032},
-        travelMode: google.maps.TravelMode.DRIVING
-    };
-
-    directionsService.route(request, function(result, status) {
-        if (status === 'OK') {
-            directionsRenderer.setDirections(result);
-        } else {
-            alert('Ошибка при построении маршрута.');
-        }
-    });
-}
+        myMap.geoObjects.add(placemark);
+    }
+});
